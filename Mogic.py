740175909e2a9323bebe12a48a1a69bef8687033
Calvin Xu <1020806611@qq.com>
2017-07-29 02:30:25 +0800

    Update

--- Mogic.py
+++ Mogic.py
@@ -1,9 +1,22 @@
-import time
+from time import sleep
 from textwrap import dedent
-import urllib
+from urllib.error import HTTPError
+from urllib.error import URLError
 from urllib.request import urlopen
-import ssl
-context = ssl._create_unverified_context()
+from ssl import _create_unverified_context
+from sys import exit
+import webbrowser
+
+context = _create_unverified_context()
+
+print('-' * 10)
+sleep(1)
+print("Version 7.2\nCalvin Xu 制作")
+sleep(1)
+print("输入 /help 得到帮助")
+sleep(1)
+print('-' * 10)
+sleep(0.5)
 
 try:
     QUOTES_URL = "https://raw.githubusercontent.com/Calvin-Xu/Mogic/master/quotes_utf-8.txt"
@@ -16,23 +29,13 @@ try:
 
     translations = {item : a[index+1] for index, item in enumerate(a) if index % 2 == 0}
 
-
-    print('-' * 10)
-    time.sleep(1)
-    print("Version 7.1\nCalvin Xu 制作")
-    time.sleep(1)
-    print("输入 /help 得到帮助")
-    time.sleep(1)
-    print('-' * 10)
-    time.sleep(0.5)
-
     try:
         while True:
-            req = input("输入: ")
+            req = input("\n输入: ")
 
             if req == "/help" or req == "／help":
                 print('-' * 10)
-                print("""
+                print(dedent("""
                       Ver.1.0:
                       输入中文，得到长者英文语录翻译，提高知识水平
                       ( NFLS 洋文好的人多的很哪!)
@@ -40,71 +43,69 @@ try:
                       输入\"/三篇\"阅读完整蛤三篇
                       按 e 可赛艇
                       更多资源请按 r
-                      """)
+                             """))
 
-                time.sleep(2)
+                sleep(2)
 
-                print("""
+                print(dedent("""
                       Ver.2.0:
                       加入模糊搜索功能
-                      """)
-                time.sleep(0.5)
+                             """))
+                sleep(0.5)
 
-                print("""
+                print(dedent("""
                       Ver.3.0:
                       加入外挂文档功能
-                      """)
-                time.sleep(0.5)
+                             """))
+                sleep(0.5)
 
-                print("""
+                print(dedent("""
                       Ver.4.0
                       加入完整三篇, excited!
-                      """)
-                time.sleep(0.5)
+                             """))
+                sleep(0.5)
 
-                print("""
+                print(dedent("""
                       Ver.5.0
                       所有文档存储于服务器端, 出了事我们跑得比谁都快
                       Ver.5.1
                       网络连接错误提示
-                      """)
-                time.sleep(0.5)
+                             """))
+                sleep(0.5)
 
-                print("""
+                print(dedent("""
                       Ver.6.0
                       完善模糊搜索功能，使用算法而非手动排除
                       Ver.6.1
                       模糊搜索 bug fix
-                      """)
+                             """))
 
-                print("""
+                print(dedent("""
                       Ver.7.0
                       全面检索谈笑风生对话支持 / 更人性化的模糊搜索
                       Ver.7.1
                       模糊搜索全面改版，更加精确
-                      """)
+                      Ver7.2
+                      优化 & bug fix
+                             """))
                 print('-' * 10)
-            else:
-                pass
 
 
-            if req == "/语录" or req == "／语录":
+            elif req == "/语录" or req == "／语录":
                 print('-' * 10)
                 print("长者 (前国家领导人江泽民) 语录 (中英对照):")
                 for ori, trans in list(translations.items()):
                     print(ori)
                     print()
                     print(trans)
-                    time.sleep(0.3)
+                    sleep(0.3)
                 print('-' * 10)
-            else:
-                pass
 
 
-            if req == "e":
+            elif req == "e":
                 for i in range(1, 65):
                     print(f"excited\n+{i}s\n")
-                    time.sleep(0.1)
+                    sleep(0.1)
 
                 print('-' * 10)
                 print("Made by NFLS - CX")
@@ -113,23 +114,28 @@ try:
                 print("Stay young, stay simple")
                 print("2017-6-26")
                 print('-' * 10)
-            else:
-                pass
 
 
-            if req == "r":
+            elif req == "r":
                 print('-' * 10)
                 print("长者 Wikipedia 词条: https://en.wikipedia.org/wiki/Jiang_Zemin")
-                time.sleep(0.5)
-                print("长者 Wikiquote 词条: https://zh.wikiquote.org/wiki/%E6%B1%9F%E6%B3%BD%E6%B0%91#.E8.A7.86.E5.AF.9F.E8.81.94.E5.B7.A5.E5.9B.BD.E6.9C.BA.E4.BA.8C.E9.99.A2")
-                time.sleep(0.5)
-                print("NFLS水上运动讨论群 ID: 527623612")
+                sleep(0.5)
+                print("\n长者 Wikiquote 词条: https://zh.wikiquote.org/wiki/%E6%B1%9F%E6%B3%BD%E6%B0%91")
+                sleep(0.5)
+                print("\nNFLS水上运动讨论群 ID: 527623612")
                 print('-' * 10)
-            else:
-                pass
+                print("\nOpen pages?")
+                answer = input("y / n: ")
+
+                if answer == "y":
+                    webbrowser.open("https://en.wikipedia.org/wiki/Jiang_Zemin")
+                    webbrowser.open("https://zh.wikiquote.org/wiki/%E6%B1%9F%E6%B3%BD%E6%B0%91")
+                    print("\n")
+                else:
+                    pass
 
 
-            if req == "/三篇" or req == "／三篇":
+            elif req == "/三篇" or req == "／三篇":
                 print("""
                       视察二院，请按 1
                       怒斥记者，请按 2
@@ -140,25 +146,22 @@ try:
                     print("----------加载中...视察二院----------\n")
                     for quote in urlopen("https://raw.githubusercontent.com/Calvin-Xu/Mogic/master/quote1.txt", context=context).readlines():
                             print(str(quote, encoding="utf-8"))
-                            time.sleep(0.5)
-
+                            sleep(0.5)
                 elif choice == "2":
                     print("----------加载中...怒斥记者----------\n")
                     for quote in urlopen("https://raw.githubusercontent.com/Calvin-Xu/Mogic/master/quote2.txt", context=context).readlines():
                             print(str(quote, encoding="utf-8"))
-                            time.sleep(1)
-
+                            sleep(1)
                 elif choice == "3":
                     print("----------加载中...谈笑风生----------\n")
                     for quote in urlopen("https://raw.githubusercontent.com/Calvin-Xu/Mogic/master/quote3.txt", context=context).readlines():
                             print(str(quote, encoding="utf-8"))
-                            time.sleep(0.5)
-
+                            sleep(0.5)
                 else:
                     pass
 
 
-            if req != "/help" and req != "/语录" and req != "e" and req != "r" and req != "/三篇" and req != "／help" and req != "／三篇" and req != "／语录":
+            elif req != "/help" and req != "/语录" and req != "e" and req != "r" and req != "/三篇" and req != "／help" and req != "／三篇" and req != "／语录":
                 print('-' * 10)
                 print(f"你输入了: \"{req}\"")
                 result = translations.get(req)
